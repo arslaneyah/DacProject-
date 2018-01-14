59ad46a360863600a1dd54c48ebd05625c9297c3
Benamer ihab <30691035+OverFlow33@users.noreply.github.com>
2018-01-14 20:22:19 +0100

    backUp 3.0

--- src/TempClass.java
+++ src/TempClass.java
@@ -1,5 +1,6 @@
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 import javafx.animation.PathTransition;
 import javafx.animation.PathTransition.OrientationType;
 import javafx.scene.image.Image;
@@ -23,164 +25,161 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
-public class TempClass extends Application 
-{
-	//Declaration
+public class Parking extends Application {
+	// Declaration
 	BorderPane main;
-	Path [] road = new Path [100];
+	Path[] road = new Path[100];
 	MoveTo moveTo;
 	StackPane pane, roads;
 	int distance[] = { 240, 400, 560, 720 };
-	int	[][] placeOrdinaire = {{240, 170, 0}, {400, 170, 0}, {560, 170, 0}, {700, 170, 0}, {240, -170, 0}, {400, -170, 0}, {560, -170, 0}, {700, -170, 0}};
-	int [][] placeHandicape = {{80, 170, 0}, {80, -170, 0}};
-	List<int[]> listePlaceHandicape = new ArrayList<int []>();
-	List<int[]> listePlaceOrdinaire = new ArrayList<int []>();
+	int[][] placeOrdinaire = { { 240, 170, 0 }, { 400, 170, 0 }, { 560, 170, 0 }, { 700, 170, 0 }, { 240, -170, 0 },
+			{ 400, -170, 0 }, { 560, -170, 0 }, { 700, -170, 0 } };
+	int[][] placeHandicape = { { 80, 170, 0 }, { 80, -170, 0 } };
+	List<int[]> listePlaceHandicape = new ArrayList<int[]>();
+	List<int[]> listePlaceOrdinaire = new ArrayList<int[]>();
 	String carType;
 	PathTransition pathTransition;
 	Button pause, play;
 	VBox buttons;
-	Group [] parking = new Group[100];
+	Group[] parking = new Group[100];
 	Scene scene;
 	ImageView car;
 	VLineTo vline1, vline2;
 	HLineTo hline1, hline2;
 
-	void sortieDirect(String type, int u)
-	{
-		//container of the road and the car
-		//creating the car 
-		parking [u] = new Group();
+	Semaphore semEnter = new Semaphore(1, true);
+	Semaphore semExit = new Semaphore(1, true);
+	Semaphore semParkH = new Semaphore(2, true);
+	Semaphore semParkN = new Semaphore(8, true);
+
+	void sortieDirect(String type, int u) {
+		// container of the road and the car
+		// creating the car
+		parking[u] = new Group();
 		car = creatCar(type);
 
-		//road.getStyleClass().add("road");
+		// road.getStyleClass().add("road");
 		moveTo = new MoveTo();
 
-		//creating the road's lines
+		// creating the road's lines
 		HLineTo hl = new HLineTo(1000);
-		//creating the road path
+		// creating the road path
 		road[u] = new Path();
 		road[u].setStrokeWidth(200);
 		road[u].getElements().clear();
 		road[u].getElements().addAll(moveTo, hl);
-		parking [u].getChildren().clear();
-		parking [u].getChildren().addAll(road[u],car);
-		//parking [u].setTranslateX(-160);
-		roads.getChildren().add(parking [u]);
+		parking[u].getChildren().clear();
+		parking[u].getChildren().addAll(road[u], car);
+		// parking [u].setTranslateX(-160);
+		roads.getChildren().add(parking[u]);
 		creatTransition(road[u], car, u);
 	}
-	void creatTransition(Path p, ImageView c, int u)
-	{
-		//hide the road stroke
+
+	void creatTransition(Path p, ImageView c, int u) {
+		// hide the road stroke
 		road[u].setStyle("-fx-opacity : 0;");
-		// creating he transition ( animation ) 
+		// creating he transition ( animation )
 		pathTransition = new PathTransition();
 		// animation speed
 		pathTransition.setDuration(Duration.millis(10000));
 		// the moving element
 		pathTransition.setNode(c);
-		// the path to move in 
+		// the path to move in
 		pathTransition.setPath(p);
-		// change the node orientation according to the path 
+		// change the node orientation according to the path
 		pathTransition.setOrientation(OrientationType.ORTHOGONAL_TO_TANGENT);
-		// looping 
-		//pathTransition.setCycleCount(Timeline.INDEFINITE);
-		// start the animation 
-		//if( buttons.getChildren().contains(pause))
+		// looping
+		// pathTransition.setCycleCount(Timeline.INDEFINITE);
+		// start the animation
+		// if( buttons.getChildren().contains(pause))
 		pathTransition.play();
 
 	}
-	Path setRoadElement(double h, double v, int o)
-	{
-		//road.getStyleClass().add("road");
+
+	Path setRoadElement(double h, double v, int o) {
+		// road.getStyleClass().add("road");
 		moveTo = new MoveTo();
 
-		//creating the road's lines
+		// creating the road's lines
 		HLineTo hl = new HLineTo(h);
 		VLineTo vl = new VLineTo(v);
-		//creating the road path
+		// creating the road path
 		road[o] = new Path();
 		road[o].setStrokeWidth(200);
-		//road.getStyleClass().add("road");
+		// road.getStyleClass().add("road");
 		road[o].getElements().clear();
 		road[o].getElements().addAll(moveTo, hl, vl);
 
 		return road[o];
 	}
-	static ImageView creatCar(String type)
-	{
+
+	static ImageView creatCar(String type) {
 		Random r;
 		ImageView car1;
-		if(type.equals("normal"))
-		{
+		if (type.equals("normal")) {
 			r = new Random();
-			car1 = new ImageView(new Image("pictures/cars/car"+r.nextInt(5)+".png"));
-		}
-		else
-		{
+			car1 = new ImageView(new Image("pictures/cars/car" + r.nextInt(5) + ".png"));
+		} else {
 			r = new Random();
-			car1 = new ImageView(new Image("pictures/cars/carh"+r.nextInt(2)+".png"));
+			car1 = new ImageView(new Image("pictures/cars/carh" + r.nextInt(2) + ".png"));
 		}
 		car1.setFitHeight(150);
 		car1.setPreserveRatio(true);
 		return car1;
 	}
-	void garer(String type, double x, double y,int u)
-	{
-		//container of the road and the car
-		//creating the car 
-		parking [u] = new Group();
+
+	void garer(String type, double x, double y, int u) {
+		// container of the road and the car
+		// creating the car
+		parking[u] = new Group();
 		car = creatCar(type);
-		//adding the element to the interface 
+		// adding the element to the interface
 		road[u] = setRoadElement(x, y, u);
-		parking [u].getChildren().clear();
-		parking [u].getChildren().addAll(road[u],car);
-		parking [u].setTranslateY(y/2);
-		System.out.println(x);
+		parking[u].getChildren().clear();
+		parking[u].getChildren().addAll(road[u], car);
+		parking[u].setTranslateY(y / 2);
 		// 720 ok
-		if(x == 560) // ok
-			parking [u].setTranslateX(-80);
-		else if(x == 400) // ok
-			parking [u].setTranslateX(-160);
-		else if(x == 240) // ok
-			parking [u].setTranslateX(-240);
-		else if(x == 80) // ok
-			parking [u].setTranslateX(-320);
-		
-		pane.getChildren().add(parking [u]);
+		if (x == 560) // ok
+			parking[u].setTranslateX(-80);
+		else if (x == 400) // ok
+			parking[u].setTranslateX(-160);
+		else if (x == 240) // ok
+			parking[u].setTranslateX(-240);
+		else if (x == 80) // ok
+			parking[u].setTranslateX(-320);
+		pane.getChildren().add(parking[u]);
 		creatTransition(road[u], car, u);
 	}
 
-	public static void main(String[] args) 
-	{
-		//starting the window
+	public static void main(String[] args) {
+		// starting the window
 		launch(args);
 	}
 
-	public void start(Stage window) 
-	{
-		listePlaceHandicape = new ArrayList<int []>(Arrays.asList(placeHandicape));
-		listePlaceOrdinaire = new ArrayList<int []>(Arrays.asList(placeOrdinaire));
-		//scene layout
+	public void start(Stage window) {
+		listePlaceHandicape = new ArrayList<int[]>(Arrays.asList(placeHandicape));
+		listePlaceOrdinaire = new ArrayList<int[]>(Arrays.asList(placeOrdinaire));
+		// scene layout
 		main = new BorderPane();
 		main.setStyle("-fx-background-image : url('pictures/background.jpg');");
 
 		pane = new StackPane();
-		//pane.setAlignment(Pos.CENTER_LEFT);
+		// pane.setAlignment(Pos.CENTER_LEFT);
 		pane.setPrefHeight(570);
 		roads = new StackPane();
 		roads.getChildren().add(pane);
-		main.setLeft(roads);    	
-		// pause/play Buttons 
+		main.setLeft(roads);
+		// pause/play Buttons
 		pause = new Button("Pause");
-		play 	= new Button("Play");
+		play = new Button("Play");
 		pause.setPrefSize(100, 30);
 		play.setPrefSize(100, 30);
 		pause.setStyle("-fx-background-radius : 30;");
 		play.setStyle("-fx-background-radius : 30;");
-		//container of the Buttons
+		// container of the Buttons
 		buttons = new VBox();
 		buttons.setAlignment(Pos.CENTER);
-		buttons.setPadding(new Insets(30,0,10,0));
+		buttons.setPadding(new Insets(30, 0, 10, 0));
 		buttons.getChildren().add(pause);
 		buttons.setStyle("-fx-background-color : #626262;");
 		main.setBottom(buttons);
@@ -195,140 +194,145 @@ public class TempClass extends Application
 		Thread t9 = new Creation();
 		Thread t10 = new Creation();
 		Thread t11 = new Creation();
-		try {
-			t1.start();
-			t1.join();
-			t2.start();
-			t2.join();
-			t3.start();
-			t3.join();
-			t4.start();
-			t4.join();
-			t5.start();
-			t5.join();
-			t6.start();
-			t6.join();
-			t7.start();
-			t7.join();
-			t8.start();
-			t8.join();
-			t9.start();
-			t9.join();
-			t10.start();
-			t10.join();
-			t11.start();
 
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+		t6.start();
+		t7.start();
+		t8.start();
+		t9.start();
+		t10.start();
+		t11.start();
+
 		// button listener
-		pause.setOnAction(e -> 
-		{
+		pause.setOnAction(e -> {
 			pathTransition.pause();
 			buttons.getChildren().clear();
 			buttons.getChildren().add(play);
 		});
 
-		play.setOnAction(e -> 
-		{
+		play.setOnAction(e -> {
 			pathTransition.play();
 			buttons.getChildren().clear();
 			buttons.getChildren().add(pause);
 		});
 
-
-
-
-		// creating the scene 
+		// creating the scene
 		scene = new Scene(main, 990, 640);
 
-		//creating the window
+		// creating the window
 		window.setTitle("parking");
 		window.setScene(scene);
 		window.setResizable(false);
 		window.show();
 	}
 
-	class Creation extends Thread
-	{
+	class Creation extends Thread {
 
-		public Creation()
-		{
+		public Creation() {
 
 		}
-		public void call(int x)
-		{
-			if(new Random().nextInt(7) == 0)
+
+		public void call(int x) {
+			if (new Random().nextInt(7) == 0)
 				carType = "handicapped";
 			else
 				carType = "normal";
-			if(carType.equals("normal"))
+			if (carType.equals("normal")) 
 			{
-
-				if(listePlaceOrdinaire.size() > 0)
+				
+				if (semParkN.availablePermits() > 0)
 				{
-					int r1 = new Random().nextInt(listePlaceOrdinaire.size());
-					garer("normal", listePlaceOrdinaire.get(r1)[0] , listePlaceOrdinaire.get(r1)[1], x);
-					listePlaceOrdinaire.remove(r1);
-				}
+					
+					try
+					{
+						semParkN.acquire();
+						if (semEnter.availablePermits() > 0) 
+						{
+							semEnter.acquire();
+							int r1 = new Random().nextInt(listePlaceOrdinaire.size());
+							garer("normal", listePlaceOrdinaire.get(r1)[0], listePlaceOrdinaire.get(r1)[1], x);
+							listePlaceOrdinaire.remove(r1);
+							Thread.sleep(10000);
+							
+							semEnter.release();
+						}
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					} finally {
+						semEnter.release();
+					}
+				} 
 				else
 				{
-					//sortieDirect("normal", x);
+					// sortieDirect("normal", x);
 				}
 
-
-			}
+			} 
 			else
 			{
-				if(listePlaceHandicape.size() > 0)
+				if (semParkH.availablePermits() > 0)
 				{
-					int r1 = new Random().nextInt(listePlaceHandicape.size());
-					garer("handicapped", listePlaceHandicape.get(r1)[0] , listePlaceHandicape.get(r1)[1], x);
-					listePlaceHandicape.remove(r1);
-				}
+					try
+					{
+						semParkH.acquire();
+						if (semEnter.availablePermits() > 0) 
+						{
+							semEnter.acquire();
+							int r1 = new Random().nextInt(listePlaceHandicape.size());
+							garer("handicapped", listePlaceHandicape.get(r1)[0], listePlaceHandicape.get(r1)[1], x);
+							listePlaceHandicape.remove(r1);
+							//Thread.sleep(10000);
+							semEnter.release();
+						}
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					} finally {
+						semEnter.release();
+					}
+				} 
 				else
 				{
-					;
-					//sortieDirect("handicapped", x);
+					// sortieDirect("normal", x);
 				}
 			}
-			// add the play button at the end of the transition 
+			// add the play button at the end of the transition
 			
 			pathTransition.statusProperty().addListener(e ->
 			{
+
 				if( e.toString().contains("STOPPED"))
 				{
 					buttons.getChildren().clear();
-					buttons.getChildren().add(play);
+					buttons.getChildren().add(play); 
 				}
-			});
+			 });
+			 
+
+		}
+
+		@Override
+		public void run() {
+			int index;
+			// storing the thread index
+			if (this.getName().length() == 8)
+				index = Integer.parseInt(this.getName().substring(this.getName().length() - 1, this.getName().length()));
+			else
+				index = Integer.parseInt(this.getName().substring(this.getName().length() - 2, this.getName().length()));
+			Platform.runLater(new Runnable()
+			{
+                @Override public void run() 
+                {
+                	
+        			call(index);
+                }
+            });
 			
 		}
-		@Override
-		public void run() 
-		{
-			int i = Integer.parseInt(this.getName().substring(this.getName().length()-1, this.getName().length()));
-			call(i);
-		}
-		
+
 	}
 
-	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
